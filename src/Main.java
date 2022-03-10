efc8e9f3ca1dd6157a0ef75889f4ddc61fc0b0b3
RadiaktiveLive <radiaktive+github@gmail.com>
2022-03-10 16:55:07 +0100

    Fase 3 amb modificacions suggerides després de la correcció del tutor

--- src/Main.java
+++ src/Main.java
@@ -4,61 +4,68 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        List<Rocket> rockets = createRockets();
-        printRocketsInformation(rockets);
-        throttleRockets(rockets, 3);
-        printRocketsCurrentPower(rockets);
-        brakeSingleRocket(findRocketById(rockets, "32WESSDS"), 5);
-        throttleSingleRocket(findRocketById(rockets, "LDSFJA32"), 7);
-        printRocketsCurrentPower(rockets);
-        throttleRockets(rockets, 15);
-        printRocketsCurrentPower(rockets);
-    }
+	public static void main(String[] args) throws Exception {
+		List<Rocket> rockets = createRockets();
+		printRocketsInformation(rockets);
+		moveRockets(rockets);
+	}
 
-    private static List<Rocket> createRockets() throws Exception {
-        List<Rocket> rockets = new ArrayList<>();
-        rockets.add(new Rocket("32WESSDS", new ArrayList<Integer>(Arrays.asList(10, 30, 80))));
-        rockets.add(new Rocket("LDSFJA32", new ArrayList<Integer>(Arrays.asList(30, 40, 50, 50, 30, 10))));
-        return rockets;
-    }
+	private static void moveRockets(List<Rocket> rockets) throws Exception {
+		throttleRockets(rockets, 3);
+		brakeSingleRocket(findRocketById(rockets, "32WESSDS"), 5);
+		throttleSingleRocket(findRocketById(rockets, "LDSFJA32"), 7);
+		printRocketsCurrentPower(rockets);
+		throttleRockets(rockets, 15);
+	}
 
-    private static void printRocketsInformation(List<Rocket> rockets) {
-        System.out.println("Rocket info:");
-        for (Rocket currentRocket : rockets) {
-            System.out.println(currentRocket);
-        }
-    }
+	private static List<Rocket> createRockets() throws Exception {
+		List<Rocket> rockets = new ArrayList<>();
+		rockets.add(new Rocket("32WESSDS", createPropellers(new Integer[]{10, 30, 80})));
+		rockets.add(new Rocket("LDSFJA32", createPropellers(new Integer[]{30, 40, 50, 50, 30, 10})));
+		return rockets;
+	}
 
-    private static void printRocketsCurrentPower(List<Rocket> rockets) {
-        System.out.println("Rocket current power:");
-        for (Rocket currentRocket : rockets) {
-            System.out.println(currentRocket.getId() + ": " + currentRocket.currentPower());
-        }
-    }
+	private static ArrayList<Integer> createPropellers(Integer[] propellers) {
+		return new ArrayList<Integer>(Arrays.asList(propellers));
+	}
 
-    private static void throttleRockets(List<Rocket> rockets, int howManyTimes) {
-        for (Rocket rocket : rockets) {
-            throttleSingleRocket(rocket, howManyTimes);
-        }
-    }
+	private static void printRocketsInformation(List<Rocket> rockets) {
+		System.out.println("Rocket info:");
+		for (Rocket currentRocket : rockets) {
+			System.out.println(currentRocket);
+		}
+	}
 
-    private static void throttleSingleRocket(Rocket rocket, int howManyTimes) {
-        for (int i = 0; i < howManyTimes; i++) {
-            rocket.throttlePropellers();
-        }
-    }
+	private static void printRocketsCurrentPower(List<Rocket> rockets) {
+		System.out.println("Rocket current power:");
+		for (Rocket currentRocket : rockets) {
+			System.out.println(currentRocket.getId() + ": " + currentRocket.currentPower());
+		}
+	}
 
-    private static void brakeSingleRocket(Rocket rocket, int howManyTimes) {
-        for (int i = 0; i < howManyTimes; i++) {
-            rocket.brakePropellers();
-        }
-    }
+	private static void throttleRockets(List<Rocket> rockets, int howManyTimes) {
+		for (Rocket rocket : rockets) {
+			throttleSingleRocket(rocket, howManyTimes);
+		}
+		printRocketsCurrentPower(rockets);
+	}
 
-    public static Rocket findRocketById(List<Rocket> rockets, String rocketToFind) {
-        return rockets.stream()
-                .filter(rocket -> rocketToFind.equals(rocket.getId()))
-                .findFirst()
-                .orElse(null);
-    }
+	private static void throttleSingleRocket(Rocket rocket, int howManyTimes) {
+		for (int i = 0; i < howManyTimes; i++) {
+			rocket.throttlePropellers();
+		}
+	}
+
+	private static void brakeSingleRocket(Rocket rocket, int howManyTimes) {
+		for (int i = 0; i < howManyTimes; i++) {
+			rocket.brakePropellers();
+		}
+	}
+
+	public static Rocket findRocketById(List<Rocket> rockets, String rocketToFind) throws Exception {
+		return rockets.stream()
+				.filter(rocket -> rocketToFind.equals(rocket.getId()))
+				.findFirst()
+				.orElseThrow(() -> new Exception("No s'ha trobat el coet"));
+	}
 }
--- src/Propeller.java
+++ src/Propeller.java
@@ -1,36 +1,38 @@
 public class Propeller {
 
-    private int maxPower;
-    private int currentPower = 0;
-    private static final int POWER = 10;
+	private int maxPower;
+	private int currentPower = 0;
+	private static final int POWER_STEP = 10;
 
-    public Propeller(int maxPower) throws Exception {
-        checkMaxPower(maxPower);
-        this.maxPower = maxPower;
-    }
+	public Propeller(int maxPower) throws Exception {
+		checkMaxPower(maxPower);
+		this.maxPower = maxPower;
+	}
 
-    private void checkMaxPower(int maxPower) throws Exception {
-        if (maxPower <= 0) throw new Exception("La potència ha de ser superior a 0");
-    }
+	private void checkMaxPower(int maxPower) throws Exception {
+		if (maxPower <= 0) throw new Exception("La potència ha de ser superior a 0");
+	}
 
-    public int getCurrentPower() {
-        return currentPower;
-    }
+	public int getCurrentPower() {
+		return currentPower;
+	}
 
-    public void throttle() {
-        if ((currentPower + POWER) <= maxPower) {
-            currentPower += POWER;
-        }
-    }
+	public void throttle() {
+		currentPower += POWER_STEP;
+		if (currentPower > maxPower) {
+			currentPower = maxPower;
+		}
+	}
 
-    public void brake() {
-        if ((currentPower - POWER) >= 0) {
-            currentPower -= POWER;
-        }
-    }
+	public void brake() {
+		currentPower -= POWER_STEP;
+		if (currentPower < 0) {
+			currentPower = 0;
+		}
+	}
 
-    @Override
-    public String toString() {
-        return "" + maxPower;
-    }
+	@Override
+	public String toString() {
+		return "" + maxPower;
+	}
 }
